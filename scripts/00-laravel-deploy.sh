07ade84196e1f6c2fc282f3efd9544d9150eedc1
vominhtridz <trilove151@gmail.com>
2024-11-27 18:48:37 +0700

    1

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -9,7 +9,12 @@ echo "Caching routes..."
 php artisan route:cache
 echo "optimize clear...."
 php artisan optimize:clear
-
+echo "Clearing view cache..."
+php artisan view:clear
+echo "Clearing compiled views..."
+php artisan view:cache
+echo "Clearing compiled assets..."
+composer dump-autoload
 echo "Running migrations..."
 php artisan migrate --force
 php artisan db:seed --class=RolePermissionSeeder
